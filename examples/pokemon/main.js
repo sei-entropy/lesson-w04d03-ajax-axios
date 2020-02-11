a2713f003dffcfdc82976b24708ce31b6caeb57d
aLi <aaljamid@emich.edu>
2020-02-11 09:03:23 -0500

    my submission to the lesson w4d3

--- examples/pokemon/main.js
+++ examples/pokemon/main.js
@@ -1,6 +1,5 @@
 // We loaded axios library in our html file
 // Lets make an ajax call with axios
-console.log('Pokemon');
 
 // method: get
 // url: 'https://pokeapi.co/api/v2/pokemon/1';
@@ -20,3 +19,83 @@ console.log('Pokemon');
 // 1. make another fucntion like one that we create but it will create for you li each time
 // 2. each li will has the p && img tags to have the pokemon name && image at least
 // 3. add more details to the li like: height && weight && base_experience
+
+const pokemonList = $(".item");
+function getPokemonsAll() {
+  for (let i = 0; i <= 5; i++) {
+    axios({
+      method: "get",
+      url: `https://pokeapi.co/api/v2/pokemon/${i + 1}`
+    })
+      .then(res => {
+        const currentPokemonName = $(pokemonList[i].children[0]);
+        const currentPokemonImage = $(pokemonList[i].children[1]);
+        const currentName = res.data.name;
+        console.log(currentName);
+        const imageUrl = `https://pokeres.bastionbot.org/images/pokemon/${i +
+          1}.png`;
+        currentPokemonName.text(currentName);
+        currentPokemonImage.attr("src", imageUrl);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
+
+function getPokemons1() {
+  axios({
+    method: "get",
+    url: `https://pokeapi.co/api/v2/pokemon/1`
+  })
+    .then(res => {
+      const currentPokemonName = $(pokemonList[0].children[0]);
+      const currentPokemonImage = $(pokemonList[0].children[1]);
+      const currentName = res.data.name;
+      console.log(currentName);
+      const imageUrl = `https://pokeres.bastionbot.org/images/pokemon/1.png`;
+      currentPokemonName.text(currentName);
+      currentPokemonImage.attr("src", imageUrl);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function getPokemons2() {
+  axios({
+    method: "get",
+    url: `https://pokeapi.co/api/v2/pokemon/1`
+  })
+    .then(res => {
+      const currentPokemonName = $(pokemonList[1].children[0]);
+      const currentPokemonImage = $(pokemonList[1].children[1]);
+      const currentName = res.data.name;
+      console.log(currentName);
+      const imageUrl = `https://pokeres.bastionbot.org/images/pokemon/2.png`;
+      currentPokemonName.text(currentName);
+      currentPokemonImage.attr("src", imageUrl);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function getPokemons3() {
+  axios({
+    method: "get",
+    url: `https://pokeapi.co/api/v2/pokemon/1`
+  })
+    .then(res => {
+      const currentPokemonName = $(pokemonList[2].children[0]);
+      const currentPokemonImage = $(pokemonList[2].children[1]);
+      const currentName = res.data.name;
+      console.log(currentName);
+      const imageUrl = `https://pokeres.bastionbot.org/images/pokemon/3.png`;
+      currentPokemonName.text(currentName);
+      currentPokemonImage.attr("src", imageUrl);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
